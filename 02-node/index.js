58979fbba4e9802514a34b16818b5910bf72b182
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2024-07-22 14:22:31 -0300

    refactor: refactoring callbacks to promises

--- 02-node/index.js
+++ 02-node/index.js
@@ -1,50 +1,65 @@
-function getUser(callback) {
-  setTimeout(() => {
-    return callback(null, {
-      id: 1,
-      name: "John",
-      birthday: new Date(),
-    });
-  }, 1000);
+function getUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({ id: 1, name: "John", birthday: new Date() });
+    }, 1000);
+  });
 }
 
-function getUserPhone(userId, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      phone: "123456789",
-      ddd: 11,
-    });
-  }, 2000);
+function getUserPhone(userId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({ phone: "92345-6789", ddd: 11 });
+    }, 2000);
+  });
 }
 
-function getUserAddress(userId, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      street: "Av. Paulista",
-      number: 123,
-    });
-  }, 2000);
+function getUserAddress(userId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve({
+        street: "Av. Paulista",
+        number: 123,
+      });
+    }, 2000);
+  });
 }
 
-getUser((error, user) => {
-  if (error) {
-    console.error("Error on get user", error);
-    return;
-  }
+const user = getUser();
 
-  getUserPhone(user.id, (error1, phone) => {
-    if (error1) {
-      console.error("Error on get user phone", error);
-      return;
-    }
-    getUserAddress(user.id, (error2, address) => {
-      if (error2) {
-        console.error("Error on get user address", error);
-        return;
-      }
+user.then((user) => {
+  return getUserPhone(user.id).then((phone) => {
+    return getUserAddress(user.id).then((address) => {
       console.log(
-        `Nome: ${user.name}, Endereço: ${address.street}, ${address.number}, Telefone: (${phone.ddd}) ${phone.phone}`
+        `
+        Nome: ${user.name}, 
+        Endereço: ${address.street}, ${address.number}, 
+        Telefone: (${phone.ddd}) ${phone.phone}
+        `
       );
     });
   });
 });
+
+// getUser((error, user) => {
+//   if (error) {
+//     console.error("Error on get user", error);
+//     return;
+//   }
+
+//   getUserPhone(user.id, (error1, phone) => {
+//     if (error1) {
+//       console.error("Error on get user phone", error);
+//       return;
+//     }
+//     getUserAddress(user.id, (error2, address) => {
+//       if (error2) {
+//         console.error("Error on get user address", error);
+//         return;
+//       }
+//       console.log(
+//         `Nome: ${user.name}, Endereço: ${address.street}, ${address.number}, Telefone: (${phone.ddd}) ${phone.phone}`
+//       );
+//     });
+//   });
+// });
